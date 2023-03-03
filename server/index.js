ee91bcf3b2dae862621d9a1eef13c936512deaf6
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-03-03 17:55:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,3 +3,10 @@ import * as dotenv from 'dotenv';
 import cors from 'cors';
 
 const express = require("express");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
