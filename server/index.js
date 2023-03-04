deb835000f5adfdfd040d10ef755dbc46ac764b7
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-03-04 13:22:38 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,4 +10,16 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
-// app.get()z
+app.get('/', (req, res) => {
+    res.send({ message: 'Hello World'});
+})
+
+const startServer = async () => {
+    try {
+        //  connect to the database
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
