9885b2a5e6d3378c013aa859f9af2ad99d85c3c3
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-03-10 16:24:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './mongodb/connect'
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./mongodb/connect";
 
 const express = require("express");
 
@@ -9,18 +8,19 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: "50mb" }));
 
-app.get('/', (req, res) => {
-    res.send({ message: 'Hello World'});
-})
+app.get("/", (req, res) => {
+  res.send({ message: "Hello World" });
+});
 
 const startServer = async () => {
-    try {
-        connectDB(process.env.MONGODB_URL)
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("Server has started on port http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
