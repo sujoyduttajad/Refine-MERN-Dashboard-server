1d5a7a7a1a3a880fc3889d9eb89a400377fe68a5
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-03-13 14:02:44 +0530

    Update property.controller.js

---
+++ controllers/property.controller.js
@@ -0,0 +1,20 @@
+import User from "../mongodb/models/user";
+import Property from "../mongodb/models/property";
+
+const getAllProperties = async (req, res) => {};
+
+const getPropertyDetail = async (req, res) => {};
+
+const createProperty = async (req, res) => {};
+
+const updateProperty = async (req, res) => {};
+
+const deleteProperty = async (req, res) => {};
+
+export {
+  getAllProperties,
+  getPropertyDetail,
+  createProperty,
+  updateProperty,
+  deleteProperty,
+};
