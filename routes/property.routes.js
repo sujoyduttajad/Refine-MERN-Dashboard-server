563143e9318c95f86c66ac3259a3622d4c79b984
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-03-14 13:14:57 +0530

    Update property.routes.js

--- routes/property.routes.js
+++ routes/property.routes.js
@@ -8,3 +8,11 @@ import {
 } from "../controllers/property.controller.js";
 
 const router = express.Router();
+
+router.route("/").get(getAllProperties);
+router.route("/:id").get(getPropertyDetail);
+router.route("/").post(createProperty);
+router.route("/:id").patch(updateProperty);
+router.route("/:id").delete(deleteProperty);
+
+export default router;
