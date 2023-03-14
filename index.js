d0f1f461ab27cc1d2a484c667e97c78c2a74e7e3
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-03-14 13:45:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-import express from 'express';
+import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./mongodb/connect.js";
-
+import userRouter from "./routes/user.routes.js";
+import propertyRouter from "./routes/property.routes.js";
 
 dotenv.config();
 
@@ -14,6 +15,10 @@ app.get("/", (req, res) => {
   res.send({ message: "Hello World" });
 });
 
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/properties", propertyRouter);
+
+// jump start the server
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
