67d27c44ee8754e3efd2f9e3b39ae0b22506b1d8
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-03-25 19:02:38 +0530

    Update property.js

--- mongodb/models/property.js
+++ mongodb/models/property.js
@@ -5,7 +5,7 @@ const PropertySchema = new mongoose.Schema({
   description: { type: String, required: true },
   propertyType: { type: String, required: true },
   location: { type: String, required: true },
-  price: { type: String, required: true },
+  price: { type: Number, required: true },
   photo: { type: String, required: true },
   creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
