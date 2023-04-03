d5cee0178b498d33355deeccf608234939f7994d
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2023-04-03 15:52:13 +0530

    Update property.controller.js

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -107,7 +107,30 @@ const createProperty = async (req, res) => {
 
 const updateProperty = async (req, res) => {};
 
-const deleteProperty = async (req, res) => {};
+const deleteProperty = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const propertyToDelete = await Property.findById({
+      _id: id,
+    }).populate("creator");
+
+    if (!propertyToDelete) throw new Error("Property not found");
+
+    const session = await mongoose.startSession();
+    session.startTransaction();
+    // remove the property
+    propertyToDelete.remove({ session });
+    // remove the creator from the property
+    propertyToDelete.creator.allProperties.pull(propertyToDelete);
+    // remove the property from the creator
+    await propertyToDelete.creator.save({ session });
+    await session.commitTransaction();
+
+    res.status(200).json({ message: "Property deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
 export {
   getAllProperties,
